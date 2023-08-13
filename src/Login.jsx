4b4bce64c71060b63c67ee69be15e7320d423d29
Choper03 <csutucdeleon@gmail.com>
2023-08-13 10:43:43 -0600

    Boton

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,34 +1,39 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const LoginForm = ({entrar}) => {
+const LoginForm = ({ entrar }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); // Nuevo estado
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    validateFields(newEmail, password); // Validar campos al cambiar el correo
   };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    validateFields(email, newPassword); // Validar campos al cambiar la contraseña
+  };
+
+  const validateFields = (newEmail, newPassword) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isEmailValid = emailRegex.test(newEmail);
+    const isPasswordValid =
+      newPassword.length >= 7 && /[A-Z]/.test(newPassword) && /\d/.test(newPassword);
+
+    setIsButtonDisabled(!(isEmailValid && isPasswordValid)); // Actualizar el estado del botón
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailRegex.test(email)) {
-      setErrorMessage('El correo electrónico no es válido');
-      return;
-    }
+    // Resto del código de validación
 
-    if (password.length < 7 || !/[A-Z]/.test(password) || !/\d/.test(password)) {
-      setErrorMessage('La contraseña debe tener al menos 7 caracteres, una mayúscula y un número');
-      return;
-    }
-
-    alert("Usuario Valido");
+    alert("Usuario Válido");
     setErrorMessage('');
     entrar();
   };
@@ -39,7 +44,9 @@ const LoginForm = ({entrar}) => {
       {errorMessage && <p className="text-danger">{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Correo Electrónico:</label>
+          <label htmlFor="email" className="form-label">
+            Correo Electrónico:
+          </label>
           <input
             type="email"
             className="form-control"
@@ -49,7 +56,9 @@ const LoginForm = ({entrar}) => {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Contraseña:</label>
+          <label htmlFor="password" className="form-label">
+            Contraseña:
+          </label>
           <input
             type="password"
             className="form-control"
@@ -58,7 +67,9 @@ const LoginForm = ({entrar}) => {
             onChange={handlePasswordChange}
           />
         </div>
-        <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+        <button type="submit" className="btn btn-primary" disabled={isButtonDisabled}>
+          Iniciar Sesión
+        </button>
       </form>
     </div>
   );
